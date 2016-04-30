448683036ac89c5bea5f70e79d9a2eb181d36bb9
janss001 <janss001@gmail.com>
2016-04-30 15:02:57 +0100

    Initial commit

---
+++ src/day_19/Exercise5.java
@@ -0,0 +1,18 @@
+package day_19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create a static method called allMatches. It should take a List of Strings
+ * and a Predicate<String>, and return a new List of all the values that passed the
+ * test. Test it with several examples.
+ *
+ * @author Sam Jansen
+ */
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
+}
