1588d1d04b491b9ee33f382d6619bd1a49ff31fa
janss001 <janss001@gmail.com>
2016-04-30 15:56:18 +0100

    Added test cases for allMatches()

--- src/day_19/Exercise5.java
+++ src/day_19/Exercise5.java
@@ -14,5 +14,29 @@ public class Exercise5 {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
+        String[] words = {"mary","possessed","a","little","lamb",
+                "with","fleece","as","white","as","asbestos"};
+
+        for (String word : words) {
+            list.add(word);
+        }
+
+        System.out.println("Original list: " + list.toString() + "\n");
+
+        System.out.println("Getting only short words...");
+        List<String> shortWords = StringUtils.allMatches(list,
+                s -> s.length() < 4);
+        System.out.println(shortWords.toString() + "\n");
+
+        System.out.println("Getting only words containing 'b'...");
+        List<String> wordsWithB = StringUtils.allMatches(list,
+                s -> s.contains("b"));
+        System.out.println(wordsWithB.toString() + "\n");
+
+        System.out.println("Getting only words of even length...");
+        List<String> evenLengthWords = StringUtils.allMatches(list,
+                s -> (s.length() % 2) == 0);
+        System.out.println(evenLengthWords.toString() + "\n");
+
     }
 }
