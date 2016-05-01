4a169dda05557b6a20209b153d4b0c56440e218e
janss001 <janss001@gmail.com>
2016-05-01 14:35:54 +0100

    Initial commit (incorrect solution)

---
+++ src/day_20/Exercise9.java
@@ -0,0 +1,25 @@
+package day_20;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Produce a String that is all the words concatenated together, but with commas in between.
+ * E.g., the result should be "hi,hello,...". Note that there is no comma at the beginning,
+ * before “hi”, and also no comma at the end, after the last word.
+ *
+ * @author Sam Jansen
+ */
+public class Exercise9 {
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("mary","possessed","a","little","lamb",
+                "with","fleece","as","white","as","asbestos");
+
+        System.out.println("Original list: " + words.toString());
+
+        String result = words.stream()
+                            .reduce("", (a,b) -> a + "," + b);
+        System.out.println("Concatenated string with commas: " + result);
+    }
+}
